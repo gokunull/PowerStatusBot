cc1d05770f76011c29a6e8b2fd9d1c7034c61df8
Vladyslav Zhmurko <vladyslav.zhmurko@quodfinancial.com>
2023-02-02 18:35:17 +0200

    Fix variable use

--- startup_script.sh
+++ startup_script.sh
@@ -6,5 +6,5 @@
 # */2 * * * * ${DIR}/startup_script.sh > /tmp/startup_script.log 2>&1
 
 killall python3
-python3 ${DIR}/novopower_bot.py "$URL" "$TOKEN" &
+python3 ${DIR}/novopower_bot.py "${URL}" "${TOKEN}" &
 cp ~/current_status.log ${DIR}
